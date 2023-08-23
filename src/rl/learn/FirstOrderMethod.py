b3f8b0bfcf5175138d93f7a29c8448422a0038c5
Steven Maio <stevenmaio.321@gmail.com>
2023-08-23 09:29:34 -0400

    [refact] rework Adam
    1. Adam no longer takes an FPRL instance as an argment
    2. es and pg use Adam rather than gradient ascent

--- src/rl/learn/FirstOrderMethod.py
+++ src/rl/learn/FirstOrderMethod.py
@@ -6,6 +6,9 @@ class FirstOrderMethod(ABC):
     def reset(self):
         ...
 
+    def init(self, fprl: "FixPropRepairLearn"):
+        ...
+
     @abstractmethod
     def step(self, fprl, gradient_estimate):
         ...
